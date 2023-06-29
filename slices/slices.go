1984af20d48abefae3bb78a2f3f18c511cc28696
razvan <razvan@doitmagic.com>
2023-06-29 16:13:43 +0300

    changed SameUnderlying

--- slices/slices.go
+++ slices/slices.go
@@ -2,5 +2,5 @@ package slices
 
 // SameUnderlying check if the first slice and the second slice have the same underlying array
 func SameUnderlying[T any](first, second []T) bool {
-	return cap(first) > 0 && cap(second) > 0 && &first[0:cap(first)][cap(first)-1] == &second[0:cap(second)][cap(second)-1]
+	return cap(first) > 0 && cap(second) > 0 && &first[:cap(first)][cap(first)-1] == &second[:cap(second)][cap(second)-1]
 }
